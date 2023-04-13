638710f9fce7b24667c4e5605f750685c4b9bab6
Joshua Joseph <tuf09815@temple.edu>
2023-04-13 11:58:48 -0400

    submission

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -10,20 +10,61 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    // reference to TimeBinder received onBind()
+    lateinit var timerBinder: TimerService.TimerBinder
+
+    // flag to check if activity is connected to a service
+    var isConnected = false
+
+
+    // define callbacks for when service is connected
+    val serviceConnection = object:ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder =  service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // bind to Service
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
+
+        // use IBinder to start the Service
         findViewById<Button>(R.id.playButton).setOnClickListener {
+            if(isConnected) timerBinder.start(100)
+
 
         }
 
+        // use IBinder to stop the Service
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
+            if(isConnected) timerBinder.pause()
 
         }
-        
+
+        // use IBinder to stop the Service
         findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if(isConnected) timerBinder.stop()
 
         }
+    }
+
+    // when activity is destroyed, disconnect from service
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(serviceConnection)
     }
 }
